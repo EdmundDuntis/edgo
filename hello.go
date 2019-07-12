da1628d6b2e3bd522479b84b333ad35d34f883d5
edmund_duntis <edmund_duntis@163.com>
2019-07-12 23:21:34 +0800

    update

--- hello.go
+++ hello.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"time"
 )
 
 func main() {
-	for i := 0; i < 5; i++ {
-		go func(i int) {
-			for {
-				j := i * i
-				fmt.Println(i, j)
-				time.Sleep(time.Second)
-				runtime.Gosched()
-			}
-		}(i)
-	}
-
-	select {}
-
+	fmt.Println("hello edmund.")
 }
