b2cf146f5dbf7df3000db9ac867e65250bf03177
edmund_duntis <edmund_duntis@163.com>
2020-06-25 12:42:23 +0800

    update

---
+++ warmup/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import _ "github.com/EdmundDuntis/edgo/warmup/testpkg"
+
+func main() {
+	age:=29
+	name:="test"
+	fmt.Println("my name is",name)
+	fmt.Println("my age is",age)
+
+}
---
+++ warmup/testpkg/init.go
@@ -0,0 +1,7 @@
+package testpkg
+
+import "fmt"
+
+func init()  {
+	fmt.Println("init in main")
+}
