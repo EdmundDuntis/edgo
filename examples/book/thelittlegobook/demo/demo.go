72762cf93e48526acb0635cd5bee43db91237eea
edmund_duntis <edmund_duntis@163.com>
2020-06-28 22:52:17 +0800

    update

--- examples/book/thelittlegobook/demo/demo.go
+++ examples/book/thelittlegobook/demo/demo.go
@@ -1,18 +1,21 @@
 package main
 
 import "fmt"
+import "github.com/mattn/go-sqlite3"
 
 type Saiyan struct {
 	Name    string
 }
 
 func main() {
-	a := make([]Saiyan, 10)
+	fmt.Println(sqlite3.Version())
+
+	a := make([]*Saiyan, 10)
 	//或者
 	//b := make([]*Saiyan, 10)
 
-	a0:=Saiyan{"a0"}
-	a1:=Saiyan{"a1"}
+	a0:=&Saiyan{"a0"}
+	a1:=&Saiyan{"a1"}
 	a[0]=a0
 	a[1]=a1
 	fmt.Println(a[0],a[1])
